8e5e6b1c534764189794a131a13c8dbf3144093d
Nova <technobaboo@gmail.com>
2020-07-02 23:32:42 -0500

    Added setuptools python script

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+	name='stardust_xr',
+	version='0.1',
+	description='Python module for Stardust clients and server implementations',
+	url='https://github.com/technobaboo/stardust-py',
+	author='Nova King',
+	author_email='technobaboo@gmail.com',
+	license='MIT',
+	packages=['stardust_xr'],
+	zip_safe=False
+)
